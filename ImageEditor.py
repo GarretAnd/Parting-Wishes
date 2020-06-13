e758135a1c744caddfe3dcb60b40209517048785
GarretAnd <56895614+GarretAnd@users.noreply.github.com>
2020-06-13 15:51:49 -0400

    Image Boxes Made

--- ImageEditor.py
+++ ImageEditor.py
@@ -1,15 +1,37 @@
 from PIL import Image, ImageDraw, ImageFont  # Allows us to edit images
+from textWrapper import text_wrap
 
-image = Image.open("finalPDF/Practice.png")  # initialise the drawing context with the image object as background
-draw = ImageDraw.Draw(image)
-font = ImageFont.truetype('fonts/DartmouthRuzickaGeneral/DartmouthRuzicka-Regular.ttf', size=20)
+font_size = 11
+source_image = Image.open("Assets/6 Responses.png").convert("RGBA")  # initialise the drawing with asset as background
+font = ImageFont.truetype('fonts/DartmouthRuzickaGeneral/DartmouthRuzicka-Regular.ttf', size=font_size)
+line_height = font.getsize('hg')[1]
 
-(x, y) = (50, 120)
-message = "Eureka!"
+box_img = Image.new('RGBA', (228, 197), "white")
+box_draw = ImageDraw.Draw(box_img)
+
+
+(x, y) = (0, 0)
+message = "Eureka! This text box should be all set hopefully? I really don't know what I'm doing and I really hope " \
+          "this library can save my but" \
+          "in a major way, if not this will be a tough look and a lot of manual labor that I am not excited to do. " \
+          "Hopefully it will all work... who knows" \
+          "worse comes to worse we just do it. We will make it either way and think about how happy this project made" \
+          " hana to do. She probably loves" \
+          "this shit now and is amazed at how its all possible. CS is so fucken powerful and it's so cool to be able " \
+          "to sit down and do this " \
+          "stuff for people without fear. It's just amazing what can be accomplished by a little tenacity and some " \
+          "computer science fun, am I right? "
 color = 'rgb(0, 0, 0)'  # black color
 
+lines = text_wrap(message, font, 228)
+print(lines)
 # draw the message on the background
-draw.text((x, y), message, fill=color, font=font)
-image.show()
-variable = 'finalPDF/pic2.png'
-image.save(variable)
+for line in lines:
+    box_draw.text((x, y), line, fill=color, font=font)
+    y = y + line_height
+
+y_box = 147
+source_image.paste(box_img, (x, y_box))
+source_image.show()
+variable = 'finalPDF/TestBounds2.png'
+source_image.save(variable)
---
+++ WishMaker.py
@@ -0,0 +1,34 @@
+import openpyxl  # Imports the Library Needed to work with excel documents; column C (person) and D (data)
+from PIL import Image, ImageDraw, ImageFont  # Allows us to edit images
+
+file = "Parting Wishes file.xlsx"  # Opening the excel document
+wb = openpyxl.load_workbook(file)  # and loading the worksheet I want
+sheet_name = wb['Form Responses 1']
+
+name_set = set()  # initializing empty set and empty dict
+name_dict = {}
+
+i = 2  # starting row
+while i != 3448:  # how far down the rows to go
+    name = sheet_name['C' + str(i)].value
+    response = sheet_name['D' + str(i)].value
+
+    if name not in name_set:  # if person's name is not in the set add their name to it
+        name_set.add(name)  # create an entry in the dictionary for them and
+        name_dict[name] = {response}  # add their response as a set
+    else:
+        name_dict[name].add(response)  # else add their data to already existing set in dict
+    i += 1
+
+wb.close()  # Closes file at the end
+name_keys = name_dict.keys()  # Gets every name in the file
+
+
+for index in name_keys:  # gets data associated with index
+    email = index.replace(' ', '.') + '.20@dartmouth.edu'  # makes their email from the name
+    item = name_dict[index]  # gets the set of data associated with the email and the size of it
+    size = len(item)
+    end = size % 6  # Tells us what type of letter we need
+
+    for response in item:
+        print(response)
